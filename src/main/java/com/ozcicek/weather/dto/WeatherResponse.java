51af51aded2d62494e7bfdab85936dd10be00f66
sedaozcicek <90seda09@gmail.com>
2024-04-28 14:15:43 +0300

    [weather-3] - DB connection

--- src/main/java/com/ozcicek/weather/dto/WeatherResponse.java
+++ src/main/java/com/ozcicek/weather/dto/WeatherResponse.java
@@ -1,5 +1,8 @@
 package com.ozcicek.weather.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record WeatherResponse(
         Request request,
         Location location,
