9866d9638b467ef07aa2fb9b325e36a92c92198a
sedaozcicek <90seda09@gmail.com>
2024-04-30 01:14:52 +0300

    [weather-7] - cityName Validator has been created

--- src/main/java/com/ozcicek/weather/controller/WeatherController.java
+++ src/main/java/com/ozcicek/weather/controller/WeatherController.java
@@ -1,14 +1,18 @@
 package com.ozcicek.weather.controller;
 
+import com.ozcicek.weather.controller.validator.CityNameConstraint;
 import com.ozcicek.weather.dto.WeatherDto;
 import com.ozcicek.weather.service.WeatherService;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/api/weather")
+@Validated
 public class WeatherController {
 
     private final WeatherService weatherService;
@@ -18,7 +22,7 @@ public class WeatherController {
     }
 
     @RequestMapping("/{city}")
-    public ResponseEntity<WeatherDto> getWeather(@PathVariable("city") String city){
+    public ResponseEntity<WeatherDto> getWeather(@PathVariable("city") @CityNameConstraint @NotBlank String city){
         return ResponseEntity.ok(weatherService.getWeatherByCityName(city));
     }
 }
